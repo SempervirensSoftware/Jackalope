d3c66156277b3e72b963df0b519b951d15ffb1e8
Peter Terrill <peter.terrill@gmail.com>
2012-12-24 21:41:39 -0800

    got the helper expanding and collapsing properly

--- Jackalope/View/PTKeyboardHelper.m
+++ Jackalope/View/PTKeyboardHelper.m
@@ -10,12 +10,14 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface PTKeyboardHelper (){
-    UIButton *_keyboardButton;
-    UIButton *_searchButton;
+    UIButton        *_keyboardButton;
+    UIButton        *_searchButton;
+    UITextField     *_searchTextField;
     
-    CGRect  _fullFrame;
-    CGRect  _collapsedFrame;
-    BOOL    _isCollapsed;
+    NSDictionary    *_expandedFrames;
+    NSDictionary    *_collapsedFrames;
+    
+    BOOL            _isCollapsed;
 }
 @end
 
@@ -24,6 +26,9 @@
 
 const int h_padding = 10;
 const int v_padding = 5;
+
+NSString *const kMainFrame = @"main";
+NSString *const kKeyboardFrame = @"keyboard";
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -37,32 +42,40 @@ const int v_padding = 5;
     [_searchButton addTarget:self action:@selector(searchButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [_searchButton sizeToFit];
 
+    // setup the frames for the expanded view
     CGFloat height = (v_padding + MAX(_searchButton.frame.size.height, _keyboardButton.frame.size.height) + v_padding);
-    _fullFrame = frame;
-    _fullFrame.size.height = height;
+    CGRect fullFrame = frame;
+    fullFrame.size.height = height;
+    CGFloat expandedKeyboardX = (fullFrame.size.width - h_padding - _keyboardButton.frame.size.width);
+    CGRect expandedKeyboardFrame = CGRectMake(expandedKeyboardX, v_padding, _keyboardButton.frame.size.width, _keyboardButton.frame.size.height);
+    _expandedFrames = @{kMainFrame:[NSValue valueWithCGRect:fullFrame], kKeyboardFrame:[NSValue valueWithCGRect:expandedKeyboardFrame]};
     
+    // setup the search button frame
+    CGRect searchFrame = CGRectMake(h_padding, v_padding, _searchButton.frame.size.width, _searchButton.frame.size.height);
+    _searchButton.frame = searchFrame;
+    
+    // setup up the frames for the collapsed view
     CGFloat collapsedWidth = (h_padding + _searchButton.frame.size.width + h_padding + _keyboardButton.frame.size.width + h_padding);
     CGFloat collapsedX = (frame.origin.x + frame.size.width - collapsedWidth);
-    _collapsedFrame = CGRectMake(collapsedX, frame.origin.y, collapsedWidth, height);
+    CGRect collapsedFrame = CGRectMake(collapsedX, frame.origin.y, collapsedWidth, height);
+    CGFloat collapsedKeyboardX = (collapsedFrame.size.width - h_padding - _keyboardButton.frame.size.width);
+    CGRect collapsedKeyboardFrame = CGRectMake(collapsedKeyboardX, v_padding, _keyboardButton.frame.size.width, _keyboardButton.frame.size.height);
+    _collapsedFrames = @{kMainFrame:[NSValue valueWithCGRect:collapsedFrame], kKeyboardFrame:[NSValue valueWithCGRect:collapsedKeyboardFrame]};
+    _keyboardButton.frame = collapsedKeyboardFrame;
     _isCollapsed = YES;
     
-    self = [super initWithFrame:_collapsedFrame];
+    self = [super initWithFrame:collapsedFrame];
     if (self) {
-        self.backgroundColor = [UIColor colorWithRed:(229.0/255.0) green:(238.0/255.0) blue:(247.0/255.0) alpha:0.9f];
+        self.backgroundColor = [UIColor colorWithRed:(244.0/255.0) green:(244.0/255.0) blue:(244.0/255.0) alpha:1.f];
         CALayer* layer = self.layer;
         [layer setMasksToBounds:YES];
-        [layer setCornerRadius:12];
+        [layer setCornerRadius:8];
         [layer setBorderWidth:0.5f];
 
-        // show the find button
-        CGRect searchFrame = CGRectMake(h_padding, v_padding, _searchButton.frame.size.width, _searchButton.frame.size.height);
-        _searchButton.frame = searchFrame;
+        // show the search button
         [self addSubview:_searchButton];
 
-        // show the keyboard button
-        CGFloat hideX = searchFrame.origin.x + searchFrame.size.width + h_padding;
-        CGRect hideButtonFrame = CGRectMake(hideX, v_padding, _keyboardButton.frame.size.width, _keyboardButton.frame.size.height);
-        _keyboardButton.frame = hideButtonFrame;
+        // show the keyboard button        
         [self addSubview:_keyboardButton];
     }
     return self;
@@ -70,18 +83,27 @@ const int v_padding = 5;
 
 -(void) searchButtonPressed:(id)sender {
     if (_isCollapsed){
-        CGFloat keyboardButtonX = (_fullFrame.origin.x + _fullFrame.size.width - h_padding - _keyboardButton.frame.size.width);
-        //CGRect keyboardFrame
-        [UIView animateWithDuration:0.5 animations:^{
-            self.frame = _fullFrame;
-//            _keyboardButton.frame.origin.x = keyboardButtonX;
-        }];
+        CGRect viewFrame = [_expandedFrames[kMainFrame] CGRectValue];
+        CGRect keyboardFrame = [_expandedFrames[kKeyboardFrame] CGRectValue];
         _isCollapsed = NO;
-    } else {
+        
         [UIView animateWithDuration:0.5 animations:^{
-            self.frame = _collapsedFrame;
+            self.frame = viewFrame;
+            _keyboardButton.frame = keyboardFrame;
         }];
+    } else {
+        CGRect viewFrame = [_collapsedFrames[kMainFrame] CGRectValue];
+        CGRect keyboardFrame = [_collapsedFrames[kKeyboardFrame] CGRectValue];
         _isCollapsed = YES;
+        
+        [UIView animateWithDuration:0.5 animations:^{
+            self.frame = viewFrame;
+            _keyboardButton.frame = keyboardFrame;
+        } completion:^(BOOL finished) {
+            if (finished){
+                
+            }
+        }];
     }
 }
 
